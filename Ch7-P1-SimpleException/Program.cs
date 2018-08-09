c9294d8f56644634ede9251fbc159196c9212cf6
Ammar Innowi <ammar.shaukat@innowi.com>
2018-08-09 16:00:39 +0500

    first commit from head office

---
+++ Ch7-P1-SimpleException/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch7_P1_SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Simple Exception  ");
+        }
+    }
+}
